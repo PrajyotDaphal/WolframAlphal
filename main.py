32a41eb0cbef38fad84d35950b149fd7b711cead
Beta_Dext <87372653+Unkown-Bug@users.noreply.github.com>
2023-11-05 02:06:24 +0530

    main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from SpeechRecognition import *
 from wolfram import *
 
 if __name__ == "__main__":
-    startup()
-    wishMe()
     while True:
     # if 1:
-        query = takeCommand().lower()
+        query = Listen().lower()
 
      # Logic for executing tasks based on query
         if 'question' in query:
                 wolfram(query)
+        else:
+            Say("NO Data!!!")
